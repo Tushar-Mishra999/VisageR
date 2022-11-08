14674368048e9745c88ffcb751ac8b039dcdee30
Tushar-Mishra999 <75738710+Tushar-Mishra999@users.noreply.github.com>
2022-11-08 22:40:23 +0530

    Selfie registration complete

--- lib/screens/auth/selfie_upload.dart
+++ lib/screens/auth/selfie_upload.dart
@@ -10,65 +10,80 @@ import 'package:provider/provider.dart';
 
 import '../../provider/user_provider.dart';
 
-class SelfieUpload extends StatelessWidget {
+class SelfieUpload extends StatefulWidget {
   SelfieUpload({Key? key}) : super(key: key);
-  File? image;
-  late final imageFile;
+
+  @override
+  State<SelfieUpload> createState() => _SelfieUploadState();
+}
+
+class _SelfieUploadState extends State<SelfieUpload> {
+  XFile? image;
+
+  //imageFile;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     final userProvider = Provider.of<UserProvider>(context, listen: false);
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: image == null
-            ? Center(
-                child: RoundedButton(
-                    title: 'Take selfie',
-                    size: size,
-                    func: () async {
-                      try {
-                        final image = await ImagePicker().pickImage(
-                            source: ImageSource.camera,
-                            preferredCameraDevice: CameraDevice.front);
-                        if (image == null) {
-                          return;
-                        }
-                        imageFile = File(image.path);
-                      } on PlatformException catch (e) {
-                        Fluttertoast.showToast(
-                            msg: "Please give necessary permissions");
+      body: image == null
+          ? Center(
+              child: RoundedButton(
+                  title: 'Take selfie',
+                  size: size,
+                  func: () async {
+                    try {
+                      image = (await ImagePicker().pickImage(
+                          source: ImageSource.camera,
+                          preferredCameraDevice: CameraDevice.front));
+                      if (image == null) {
+                        return;
                       }
-                    }))
-            : Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                RoundedButton(
-                    title: 'Retake selfie',
-                    size: size,
-                    func: () async {
-                      try {
-                        final image = await ImagePicker().pickImage(
-                            source: ImageSource.camera,
-                            preferredCameraDevice: CameraDevice.front);
-                        if (image == null) {
-                          return;
-                        }
-                        imageFile = File(image.path);
-                      } on PlatformException catch (e) {
-                        Fluttertoast.showToast(
-                            msg: "Please give necessary permissions");
+                      var length = await image!.length();
+                      if (length > 3670016) {
+                        Fluttertoast.showToast(msg: "Image size is too big.Please retry");
+                        return;
                       }
-                    }),
-                SizedBox(
-                  height: size.height * 0.05,
-                ),
-                RoundedButton(
-                    title: 'Upload selfie',
-                    size: size,
-                    func: () async {
-                      await uploadSelie(userProvider.user.name,userProvider.user.email, imageFile);
-                    })
-              ]),
-      ),
+                      setState(() {});
+                    } on PlatformException catch (e) {
+                      Fluttertoast.showToast(
+                          msg: "Please give necessary permissions");
+                    }
+                  }))
+          : Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RoundedButton(
+                        title: 'Retake selfie',
+                        size: size,
+                        func: () async {
+                          try {
+                            image = await ImagePicker().pickImage(
+                                source: ImageSource.camera,
+                                preferredCameraDevice: CameraDevice.front);
+                            if (image == null) {
+                              return;
+                            }
+                          } on PlatformException catch (e) {
+                            Fluttertoast.showToast(
+                                msg: "Please give necessary permissions");
+                          }
+                        }),
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    RoundedButton(
+                        title: 'Upload selfie',
+                        size: size,
+                        func: () async {
+                          await uploadSelie(userProvider.user.name,
+                              userProvider.user.email, image);
+                        })
+                  ]),
+            ),
     );
   }
 }
--- lib/services/upload_selfie.dart
+++ lib/services/upload_selfie.dart
@@ -1,18 +1,62 @@
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 
-Future<void> uploadSelie(
-    String username, String email, dynamic imageFile) async {
+Future<void> uploadSelie(String username, String email, XFile? image) async {
   try {
-    http.Response res = await http.post(
-      Uri.parse(
-          'https://kf6mue8kw2.execute-api.us-east-1.amazonaws.com/default/register/admin/delete-product'),
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode({'name': username, 'snu_id': email, 'image': imageFile}),
-    );
+    // http.Response res = await http.post(
+    //   Uri.parse(
+    //       'https://kf6mue8kw2.execute-api.us-east-1.amazonaws.com/default/register'),
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: json.encode({'name': username, 'snu_id': email, 'image': image}),
+    // );
+    // Fluttertoast.showToast(msg: res.body);
+
+    // var headers = {'Content-Type': 'application/json'};
+    // var request = http.MultipartRequest(
+    //     'POST',
+    //     Uri.parse(
+    //         'https://kf6mue8kw2.execute-api.us-east-1.amazonaws.com/default/register'));
+    // request.fields.addAll({
+    //   "name": username,
+    //   "snu_id": email,
+    // });
+    // request.headers.addAll(headers);
+    // request.files.add(await http.MultipartFile.fromPath('image', image!.path));
+    // http.StreamedResponse res = await request.send();
+    // print("hello");
+    // print(res);
+    // Fluttertoast.showToast(msg: res.statusCode.toString());
+
+    var stream = http.ByteStream(image!.openRead());
+    stream.cast();
+
+    var length = await image.length();
+    var uri = Uri.parse(
+        'https://kf6mue8kw2.execute-api.us-east-1.amazonaws.com/default/register');
+
+    var request = http.MultipartRequest('POST', uri);
+    request.fields['name'] = username; //username
+    request.fields['snu_id'] = email; //email
+
+    var multiport = http.MultipartFile('image', stream, length); //image
+
+    request.files.add(multiport);
+    var res = await request.send();
+    if (res.statusCode == 200) {
+      Fluttertoast.showToast(msg: "Uploaded Succesfully");
+
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(email)
+          .update({"isSelfieUploaded": true});
+    } else {
+      Fluttertoast.showToast(msg: "Something went wrong please retry");
+    }
   } catch (e) {
     Fluttertoast.showToast(msg: e.toString());
   }
