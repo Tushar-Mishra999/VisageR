ee0daa2a8198356d9722771dafd1c8672519cb7e
Tushar-Mishra999 <75738710+Tushar-Mishra999@users.noreply.github.com>
2022-12-11 15:55:20 +0530

    added app_icon

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 Color khomecolor = Colors.deepPurple.shade600;
+double roomLatitude = 28.52664380538595;
+double roomLongitude = 77.57617822297591;
--- lib/services/mark_attendance.dart
+++ lib/services/mark_attendance.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'package:facialrecognition_attendance/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
@@ -9,6 +11,34 @@ Future markAttendance(
   try {
     var bytes = await image!.readAsBytes();
     String base64Image = base64Encode(bytes);
+    var permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        Fluttertoast.showToast(msg: 'Location Permission Denied');
+        return;
+      }
+    }
+
+    final currentPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+
+    double distanceInMeters = 0;
+    distanceInMeters = Geolocator.distanceBetween(
+      roomLatitude,
+      roomLongitude,
+      currentPosition.latitude,
+      currentPosition.longitude,
+    );
+
+    Fluttertoast.showToast(msg: "DIstance is $distanceInMeters");
+    if (distanceInMeters / 1000 < 0.5) {
+      Fluttertoast.showToast(
+          msg: "Location outside of B-block",
+          toastLength: Toast.LENGTH_LONG,
+          backgroundColor: Colors.purple);
+      return;
+    }
 
     http.Response res = await http.post(
       Uri.parse(
@@ -25,20 +55,17 @@ Future markAttendance(
           msg: "Attendance marked",
           toastLength: Toast.LENGTH_LONG,
           backgroundColor: Colors.purple);
-    }
-    else if (res.statusCode == 401) {
+    } else if (res.statusCode == 401) {
       Fluttertoast.showToast(
           msg: "Face not recognised",
           toastLength: Toast.LENGTH_LONG,
           backgroundColor: Colors.purple);
-    }
-    else if (res.statusCode == 403) {
+    } else if (res.statusCode == 403) {
       Fluttertoast.showToast(
           msg: "Attendance is currently disabled",
           toastLength: Toast.LENGTH_LONG,
           backgroundColor: Colors.purple);
-    } 
-    else {
+    } else {
       Fluttertoast.showToast(msg: "Something went wrong please retry");
     }
   } catch (e) {
